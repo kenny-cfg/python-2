c4ad5c360cb050a6753f6ca472d4f2e2eeeae3f2
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-09-14 20:12:49 +0100

    Rename a symbol

--- function.py
+++ function.py
@@ -1,12 +1,12 @@
-def getGreeting(name):
+def get_greeting(name):
   return f'Hello {name}'
 
 def function_with_no_return():
   pass
 
-# name = "Erika"
-# greeting = getGreeting(name)
-# print(greeting)
+name = "Erika"
+greeting = get_greeting(name)
+print(greeting)
 
 return_value = function_with_no_return()
 print(return_value)
